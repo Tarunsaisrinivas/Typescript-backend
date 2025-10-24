e0b5f87f2882837130e52a879536eb7073a13091
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-10-24 22:19:04 +0530

    added helmet package for the security of application

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@ import express, { Express, Request, Response } from "express";
 import {config} from "dotenv";
 import connectDB from "./utils/db";
 import bookRoute from "./routes/bookRoute";
+import helmet from "helmet";
 config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.get("/", (req:Request, res:Response) => {
   res.send("Server is running smoothly 🚀");
 });
 app.use("/api/books", bookRoute);
-
 app.listen(port, () => {
   connectDB();
   console.log(`✅ Server is running at http://localhost:${port}`);
