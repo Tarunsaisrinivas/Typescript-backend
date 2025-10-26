590d0baff1e899ebeeeb0c7d54d6e670dcb25a10
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-10-26 15:08:04 +0530

    created a CREATE logic for books which is from crud operation

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,5 +1,34 @@
 import { Request, Response } from "express";
-
-export const getBooks = (req:Request, res:Response) => {
-    res.json({success:false, message:"No books found"});
+import Book from "../models/book";
+interface IResponse {
+  success: boolean;
+  message: string;
+  data?: any;
 }
+export const getBooks = (req: Request, res: Response) => {
+  res.json({ success: false, message: "Books found" } as IResponse);
+};
+
+export const addBook = async (req: Request, res: Response) => {
+  const { name, author, publishedYear, description } = req.body;
+
+  try {
+    const book = Book.create({
+      name,
+      author,
+      publishedYear,
+      description,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Book added successfully",
+      data: book,
+    } as IResponse);
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+      errorMessage: error.message,
+    });
+  }
+};
