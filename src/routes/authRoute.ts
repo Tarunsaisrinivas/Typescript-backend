4733291c5c11f46104b01bfeb57a7550cc66d0fc
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-11-19 10:59:12 +0530

    added the signup path in authRoute file

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,7 +1,9 @@
 import {Router} from 'express';
+import { signup } from '../controllers/authController';
 
 const authRouter = Router();
 
+authRouter.post('/signup', signup);
 
 
 export default authRouter;
