0cc71eb4dae9190489166db34baac6a5e98795d3
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-11-24 22:01:31 +0530

    verify token middleware with jwt verify

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -1,16 +1,31 @@
 import { Request,Response,NextFunction } from "express";
+import { IResponse } from "../controllers/bookController";
+import jwt,{ Secret } from "jsonwebtoken";
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     try {
         const cookie = req.headers.cookie;
-        console.log("The cookie is:",cookie);
+        // console.log("The cookie is:",cookie);
         const token = cookie?.split("=")[1];        
-        console.log("The token is:",token);
+        // console.log("The token is:",token);
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "Access denied Unauthorized",
+                data: null,
+            } as IResponse);
+        }
+       const data = jwt.verify(token, process.env.JWT_SECRET as Secret) as {id:string,role:string};
+       req.id = data.id;
+       req.role = data.role;
+       next();
+       
     } catch (error:any) {
         return res.status(500).json({
             success: false,
             message: "Server Error",
             errorMessage: error.message,
             data: null,
-        });
+        } as IResponse);
     }
 }
+export default verifyToken;
--- src/routes/bookRoute.ts
+++ src/routes/bookRoute.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { addBook, deleteBook, getBooks, updateBook } from "../controllers/bookController";
-
+import verifyToken from "../middleware/verifyToken";
 const router = Router();
 
-router.get("/get-books", getBooks);
+router.get("/get-books",verifyToken, getBooks);
 router.post('/add-book', addBook);
 router.put("/update-book/:id", updateBook);
 router.delete("/delete-book/:id", deleteBook);
