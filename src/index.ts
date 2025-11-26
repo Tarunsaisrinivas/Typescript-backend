c346cce46d1e4fc0818a2f96060a334da2a0d4ba
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-11-26 10:37:34 +0530

    updated for vercel

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
-import express, { Express, Request, Response } from "express";
-import {config} from "dotenv";
+import express, { Express } from "express";
+import { config } from "dotenv";
 import connectDB from "./utils/db";
 import bookRoute from "./routes/bookRoute";
-import helmet from "helmet";
 import authRouter from "./routes/authRoute";
-import { swaggerDocs } from "./swagger";
+import helmet from "helmet";
 config();
-
 const app: Express = express();
-const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(helmet());
-app.get("/", (req:Request, res:Response) => {
+app.get("/", (req, res) => {
   res.send("Server is running smoothly 🚀");
 });
 app.use("/api/books", bookRoute);
 app.use("/api/auth", authRouter);
-swaggerDocs(app);
-app.listen(port, () => {
-  connectDB();
-  console.log(`✅ Server is running at http://localhost:${port}`);
-  console.log(`✅ Swagger documentation is running at http://localhost:${port}/api-docs`);
-});
+connectDB();
+export default app;
